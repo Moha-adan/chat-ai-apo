efb0a218b9e37629977bc76d7b0b49218d5acb6c
moh123adan <maxamedadan169@gmail.com>
2025-04-05 14:46:25 +0300

    setting up the stream api key and secret key

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,29 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import { StreamChat } from "stream-chat";
 
 dotenv.config();
 
 const app = express();
+
+//initialize stream client
+const chatClient = StreamChat.getInstance(
+    process.env.STREAM_API_KEY!,
+    process.env.STREAM_API_SECRET!
+)
+
+//app route
+app.post("/", async (req: Request, res: Response): Promise<any> => {
+  const { name, email } = req.query;
+
+  if (!name || !email) {
+    return res.status(400).json({ message: "Name and email are required" });
+  }
+  return res
+    .status(200)
+    .json({ message: `Hello ${name}, your email is ${email}` });
+});
 
 app.use(cors());
 app.use(express.json());
